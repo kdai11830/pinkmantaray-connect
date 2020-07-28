e05eb7fc263f2bbe1a55b589e3729377d8913c26
Victoria <vnkcampbell@gmail.com>
2020-07-27 18:50:27 -0700

    Add styling fixes

--- application.js
+++ application.js
@@ -30,13 +30,13 @@ app.set('view engine', 'ejs');
 // establish mysql connection and promisify
 
 //kevin's mysql connection
-var connection = mysql.createConnection({
-	host     : 'localhost',
-	user     : 'root',
-	password : 'Jkmrhi11830!',
-	database : 'pinkmantaray_connect',
-	multipleStatements: 'true'
-});
+// var connection = mysql.createConnection({
+// 	host     : 'localhost',
+// 	user     : 'root',
+// 	password : 'Jkmrhi11830!',
+// 	database : 'pinkmantaray_connect',
+// 	multipleStatements: 'true'
+// });
 
 // var pool = mysql.createPool({
 // 	connectionLimit : 10,
@@ -51,13 +51,13 @@ var connection = mysql.createConnection({
 // vic's mysql connection
 // establish mysql connection
 
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "bearbear",
-//   database: "new_schema2",
-//   multipleStatements: "true",
-// })
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "bearbear",
+  database: "new_schema2",
+  multipleStatements: "true",
+})
 
 
 // var connection = mysql.createConnection({
--- scripts/notificationsScript.js
+++ scripts/notificationsScript.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 		console.log(idx);
 		var id = $('#invitationsTable #userId' + idx).html();
 		$("#invitationsTable #"+idx).empty();
-		$("#invitationsTable #"+idx).append('<td class="successMsg">nice.</td>');
+		// $("#invitationsTable #"+idx).append('<td class="successMsg">nice.</td>');
 		$("#invitationsTable #"+idx).append('<td class="successMsg">Invitation accepted! You can now see their information at the <a href="/">homepage</a>.</td>');
 		$("#invitationsTable #buttons"+idx).remove();
 
